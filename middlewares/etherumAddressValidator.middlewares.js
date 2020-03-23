841be1620c99400a3f54cbdde4a36509d32d1b53
w0xter <luispozogilo@gmail.com>
2020-03-23 19:44:53 +0100

    Adding new Squad's model fields and working in etherum address validator

---
+++ middlewares/etherumAddressValidator.middlewares.js
@@ -0,0 +1,53 @@
+/**
+ * Checks if the given string is an address
+ *
+ * @method isAddress
+ * @param {String} address the given HEX adress
+ * @return {Boolean}
+*/
+module.exports.isAddress = (address) => {
+	return new Promise(async (resolve, reject) => {
+		let isHex = new RegExp('^(0x)?[0-9a-f]{40}$', 'i');
+		let isHexUpper = new RegExp('^(0x)?[0-9a-f]{40}$');
+		let isHexLower = new RegExp('^(0x)?[0-9A-F]{40}$');
+		console.log("Lower:" + isHexLower.test(address) +" Length: " + address.length +  " UPPER: " + isHexUpper.test(address))
+	    if (!isHex.test(address)) {
+		 console.log("Falla isAddress" + address)
+		 console.log("RESULT: " + /^(0x)?[0-9a-f]{40}$/i.test(address))
+        	// check if it has the basic requirements of an address
+	        reject(false);
+	    } else if (isHexLower.test(address) || isHexUpper.test(address)) {
+        	// If it's all small caps or all all caps, return true
+	        resolve(true);
+	    } else {
+        	// Otherwise check each case
+	        isChecksumAddress(address).then(data => resolve(data)).catch(err => reject(err));
+	    }
+	});
+};
+
+/**
+ * Checks if the given string is a checksummed address
+ *
+ * @method isChecksumAddress
+ * @param {String} address the given HEX adress
+ * @return {Boolean}
+*/
+function isChecksumAddress(address) {
+	return new Promise((resolve, reject) => {
+    		// Check each case
+		address = address.replace('0x','');
+		let addressHash = sha3(address.toLowerCase());
+		for (let i = 0; i < 40; i++ ) {
+	        // the nth letter should be uppercase if the nth digit of casemap is 1
+        		if (
+			(parseInt(addressHash[i], 16) > 7 && address[i].toUpperCase() !== address[i]) || 
+			(parseInt(addressHash[i], 16) <= 7 && address[i].toLowerCase() !== address[i])
+			) {
+				console.log("Falla en checkSum");
+	        		reject(false);
+	        	}
+    		}
+	    	resolve(true);
+	});
+};
--- models/squad.models.js
+++ models/squad.models.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
+const etherumValidator = require('../middlewares/etherumAddressValidator.middlewares.js');
+
 const { Schema } = mongoose;
 
 const squadSchema = new Schema({
 	name:{type:String, unique:true, required:[true, 'Squad Name required']},
 	verified:{type:Boolean, required:false, default:false},
-	signUpDate: { type: Date, default: Date.now() },
+	description:{type:String, unique:false, required:[true, 'Squad description required']},
+	inviteLink:{type:String, unique:false, required:[true, 'Squad invite link is required']},
+	daoAddress:{type:String,unique:false, required:[true, 'The DAO address required']},
+	signUpDate: { type: Date, default: Date.now() }
 });
 module.exports = mongoose.model('Squad', squadSchema);
